be4e3287d98076367660b5e13d09461aa6f445c8
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-01-18 15:53:08 +0200

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) =>{
+    res.send('Home page')
+})
+
+app.listen(port, () => console.log(`Server is listening on port ${port}... `))
+
