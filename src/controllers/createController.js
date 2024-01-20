ba8ef1c1527f99e95f58ff7913f48d4206d7b6f2
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-01-20 18:27:08 +0200

    Create movie action and service

--- src/controllers/createController.js
+++ src/controllers/createController.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
+const movieService = require('../services/movieService');
+
 router.get('/create', (req, res) => {
     res.render('create')
+});
+
+router.post('/create', (req, res) => {
+   const data = req.body;
+   movieService.create(data);
+   res.send('')
 });
 
 
---
+++ src/services/movieService.js
@@ -0,0 +1,3 @@
+exports.create = (movieData) => {
+    console.log(movieData);
+};
