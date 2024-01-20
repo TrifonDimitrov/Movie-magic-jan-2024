fe9eb467dc1a7673e7580129ec460aa9b539373e
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-01-20 16:37:59 +0200

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
+
 const homeView = require('./controllers/homeController');
 const createPostView = require('./controllers/createController');
 
 
 router.use(homeView);
 router.use(createPostView);
+
+router.get('*', (req, res) => {
+    res.redirect('/404')
+});
 
 
 module.exports = router;
