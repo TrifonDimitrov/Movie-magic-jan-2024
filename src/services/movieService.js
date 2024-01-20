90ff28273fe6ea7e7ea14d922b4fcdc5f43bec4c
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-01-20 18:40:44 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,3 +1,14 @@
+const movies = [{
+title: 'Matrix',
+genre: 'action',
+director: 'Myrphy',
+year: '2133',
+imageURL: 'frfr',
+rating: '4',
+description: 'fffffffffffffffffffffffffffffffffffffffff'
+}];
+
 exports.create = (movieData) => {
     console.log(movieData);
+    movies.push(movieData);
 };
