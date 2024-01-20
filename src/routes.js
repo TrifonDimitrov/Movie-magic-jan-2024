475dbefdc2d0dea6502761e57d6e2d2d043672bf
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-01-20 15:29:32 +0200

    Add create post page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const homeView = require('./controllers/homeController');
+const createPostView = require('./controllers/createController');
 
 
 router.use(homeView);
+router.use(createPostView);
 
 
 module.exports = router;
