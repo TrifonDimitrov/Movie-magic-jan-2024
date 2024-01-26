52c94d761afc06304c04b20506f5a1ab0561f3e6
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-01-26 19:11:05 +0200

    Add create cast page

---
+++ src/controllers/castControler.js
@@ -0,0 +1,8 @@
+const router = require('express').Router(); 
+
+router.get('/create', (req, res) => {
+    res.render('cast/create');
+});
+
+
+module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const homeView = require('./controllers/homeController');
 const createPostView = require('./controllers/createController');
+const castView = require('./controllers/castControler');
 
 
 router.use(homeView);
 router.use(createPostView);
+router.use('/cast', castView);
 
 router.get('*', (req, res) => {
     res.redirect('/404')
