2f85411e817dc62aef6e9ddc42aeab9c82230383
TrifonDimitrov <dimitrov1trifon@gmail.com>
2024-02-02 17:22:16 +0200

    Hashing password on save

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,25 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     email: {
         type: String,
         required: true,
         lowercase: true,
-        
+
     },
     password: {
         type: String,
         required: true,
     }
-    
+
+});
+
+// Hash the password before saving the user to the database
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
 });
 
 const User = model('User', userSchema);
